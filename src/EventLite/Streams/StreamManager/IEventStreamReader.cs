b8c57123b03dff7ac1a668568b9772a02f8af1cf
GuzzoLM <lucasmguzzo@gmail.com>
2019-11-05 22:10:33 +0000

    Update for release 0.1.0 (#23)
    * Document public methods (#21)
    
    * Add documentation to all public methods in interfaces
    
    * Document extension methods from Mongo package
    
    * Create nuget package (#22)
    
    * Update package properties
    
    * Replace project reference by nuget package
    
    * Update package properties

--- src/EventLite/Streams/StreamManager/IEventStreamReader.cs
+++ src/EventLite/Streams/StreamManager/IEventStreamReader.cs
@@ -5,6 +5,12 @@ namespace EventLite.Streams.StreamManager
 {
     public interface IEventStreamReader
     {
+        /// <summary>
+        /// Gets an Aggregate of the specified type and with the specified unique id
+        /// </summary>
+        /// <typeparam name="TAggregate">The type of the Aggregate</typeparam>
+        /// <param name="streamId">The Aggregate's unique identifier</param>
+        /// <returns></returns>
         Task<TAggregate> GetAggregate<TAggregate>(Guid streamId) where TAggregate : IAggregateBase, new();
     }
 }
