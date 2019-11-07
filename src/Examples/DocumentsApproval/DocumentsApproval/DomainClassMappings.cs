e10a1742b63fea5b0539964bba5d4093355d02c8
GuzzoLM <lucasmguzzo@gmail.com>
2019-11-07 16:36:30 +0000

    Fix mappings

---
+++ src/Examples/DocumentsApproval/DocumentsApproval/DomainClassMappings.cs
@@ -0,0 +1,21 @@
+﻿using DocumentsApproval.Events;
+using DocumentsApproval.Model;
+using static EventLite.MongoDB.EventLiteMongoDbExtensions;
+
+namespace DocumentsApproval
+{
+    public static class DomainClassMappings
+    {
+        public static void RegisterClassSerializationMaps()
+        {
+            RegisterClassToMongo<ArtifactsUpdated>();
+            RegisterClassToMongo<DocumentApproved>();
+            RegisterClassToMongo<DocumentCreated>();
+            RegisterClassToMongo<DocumentDeleted>();
+            RegisterClassToMongo<DocumentRejected>();
+            RegisterClassToMongo<DocumentRenamed>();
+
+            RegisterClassToMongo<Document>();
+        }
+    }
+}
